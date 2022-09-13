1ae92693082102f11ba1f2a381c84f235b19cbb4
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-13 09:41:21 -0400

    just finnished putting in a little code before class done with Employee.js just need to finnish up employee test.

--- Develop/__tests__/Employee.test.js
+++ Develop/__tests__/Employee.test.js
@@ -1,5 +1,14 @@
 const Employee = require("../lib/Employee");
-
+const employee = new Employee('grant', '576', 'grant@gmail.com')
 describe("Employee", () => {
 
+});
+test('constroctor values of employee object', () => {
+    expect(epmployee.name).toBe('grant');
+    expect(epmployee.id).toBe('576');
+    expect(epmployee.email).toBe('grant@gmail.com');
+});
+
+test('do we get a name from getName', ()=>{
+    expect(employee.getName()).toBe('grant')
 });
