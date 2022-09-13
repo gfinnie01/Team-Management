b84378760f51d4ad249c7479e0f14b1c00390564
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-13 18:40:23 -0400

    finished with the index for manager but it will not work

--- Develop/index.js
+++ Develop/index.js
@@ -4,11 +4,39 @@ const Intern = require('./lib/Intern');
 const inquirer  = require('inquirer');
 const fs = require('fs');
 const teamMembers = [];
-const path = require("path")
+const path = require("path");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "template.html");
+const generateSite = require('./src/page-template')
 
-// TODO: CODE GOES HERE
+
+
+const promptMenu = () => {
+    return inquirer.prompt([
+        {
+        type: 'list',
+        name: 'menu',
+        message: 'choose one',
+        choices: ['add a engineer', 'add a intern', 'I am done with my team', 'add a manager']
+        }])
+        .then(userChoice => {
+            switch (userChoice.menu) {
+                case 'add a engineer':
+                    promptEngineer();
+                    break;
+                case 'add a intern':
+                    promptIntern();
+                    break;
+                case 'add a manager':
+                    promptManager();
+                    break;
+                default:
+                    buildTeam();
+            }
+        });
+};
 const promptManager = () => {
-return inquirer.prompt ([
+    return inquirer.prompt ([
         {
             type: 'input',
             name:  'name',
@@ -34,4 +62,5 @@ return inquirer.prompt ([
         const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
         teamMembers.push(manager);
         promptMenu();
-    })}
+    })
+}; 
