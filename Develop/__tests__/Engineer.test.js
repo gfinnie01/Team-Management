eef5f6aebcc1fb154923794552f9437e4261484e
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-13 15:28:45 -0400

    just finnished engineer test and am going to get stared with the js

--- Develop/__tests__/Engineer.test.js
+++ Develop/__tests__/Engineer.test.js
@@ -1,5 +1,22 @@
 const Engineer = require("../lib/Engineer");
-
+const engineer = new Engineer('grant', '576', 'grant@gmail.com', 'grant01' )
 describe("Engineer", () => {
 
+});
+test('constroctor values of employee object', () => {
+    expect(engineer.name).toBe('grant');
+    expect(engineer.id).toBe('576');
+    expect(engineer.email).toBe('grant@gmail.com');
+    expect(engineer.github).toBe('grant01')
+});
+
+test('do we get a name from getName', ()=>{
+    expect(engineer.getName()).toBe('grant')
+});
+
+test('do we get Id from getId', ()=> {
+    expect(engineer.getEmail()).toBe('grant@gmail.com')
+});
+test('do we get the school number from getSchool', () => {
+    expect(engineer.getGithub()).toBe('grant01')
 });
